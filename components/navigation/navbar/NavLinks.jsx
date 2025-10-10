cc3269f6e268a639e119b8d011cf9a2cdb5d662e
md-zeon <zeonrahaman5870@gmail.com>
2025-10-10 15:37:15 +0600

    Profile page user not found problem solved

--- components/navigation/navbar/NavLinks.jsx
+++ components/navigation/navbar/NavLinks.jsx
@@ -10,56 +10,51 @@ import Link from "next/link";
 import { LogIn, LogOut } from "lucide-react";
 
 const NavLinks = ({ isMobileNav = false }) => {
-  const pathname = usePathname();
-  const { data: session } = useSession();
-  const userId = session?.user?.id;
+	const pathname = usePathname();
+	const { data: session } = useSession();
+	const userId = session?.user?.id;
 
-  return (
-    <nav
-      className={cn(
-        isMobileNav &&
-          "flex justify-around border-b sticky z-50 top-0 left-0 right-0 w-full"
-      )}
-    >
-      {sidebarLinks.map((item) => {
-        const isActive =
-          (pathname.includes(item.route) && item.route.length > 1) ||
-          pathname === item.route;
+	if (!userId) return null;
 
-        if (item.route === "/profile") {
-          item.route = `${item.route}/${userId}`; // mock profile
-        }
+	return (
+		<nav className={cn(isMobileNav && "flex justify-around border-b sticky z-50 top-0 left-0 right-0 w-full")}>
+			{sidebarLinks.map((item) => {
+				const isActive = (pathname.includes(item.route) && item.route.length > 1) || pathname === item.route;
 
-        return (
-          <NavLink
-            key={item.label}
-            item={item}
-            isActive={isActive}
-            isMobileNav={isMobileNav}
-          />
-        );
-      })}
+				if (item.route === "/profile") {
+					item.route = `${item.route}/${userId}`;
+				}
 
-      {/*  Auth Buttons */}
-      {!session ? (
-        <Link
-          href="/login"
-          className="flex items-center gap-2 p-4 rounded-2xl text-gray-500 hover:bg-gray-200 dark:hover:bg-gray-700"
-        >
-          <LogIn className="h-5 w-5" />
-          <p className={cn("max-lg:hidden", isMobileNav && "hidden")}>Login</p>
-        </Link>
-      ) : (
-        <button
-          onClick={() => signOut({ callbackUrl: "/login" })}
-          className="flex items-center gap-2 p-4 rounded-2xl text-gray-500 hover:bg-gray-200 dark:hover:bg-gray-700"
-        >
-          <LogOut className="h-5 w-5" />
-          <p className={cn("max-lg:hidden", isMobileNav && "hidden")}>Logout</p>
-        </button>
-      )}
-    </nav>
-  );
+				return (
+					<NavLink
+						key={item.label}
+						item={item}
+						isActive={isActive}
+						isMobileNav={isMobileNav}
+					/>
+				);
+			})}
+
+			{/*  Auth Buttons */}
+			{!session ? (
+				<Link
+					href='/login'
+					className='flex items-center gap-2 p-4 rounded-2xl text-gray-500 hover:bg-gray-200 dark:hover:bg-gray-700'
+				>
+					<LogIn className='h-5 w-5' />
+					<p className={cn("max-lg:hidden", isMobileNav && "hidden")}>Login</p>
+				</Link>
+			) : (
+				<button
+					onClick={() => signOut({ callbackUrl: "/login" })}
+					className='flex items-center gap-2 p-4 rounded-2xl text-gray-500 hover:bg-gray-200 dark:hover:bg-gray-700'
+				>
+					<LogOut className='h-5 w-5' />
+					<p className={cn("max-lg:hidden", isMobileNav && "hidden")}>Logout</p>
+				</button>
+			)}
+		</nav>
+	);
 };
 
 export default NavLinks;
